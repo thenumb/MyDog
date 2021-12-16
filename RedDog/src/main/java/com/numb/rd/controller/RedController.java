4010dd3a5bb9252db50003b35a18705d8b404fbe
thenumb <864635061@qq.com>
2021-12-17 01:09:32 +0800

    修改maven打包成lib conf jar zip模式

--- RedDog/src/main/java/com/numb/rd/controller/RedController.java
+++ RedDog/src/main/java/com/numb/rd/controller/RedController.java
@@ -1,9 +1,6 @@
 /* Copyright © 2020 你哭时梨花带雨。 and/or its affiliates. All rights reserved. */
 package com.numb.rd.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +11,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test1")
 public class RedController {
-    @PostMapping("/test")
-    public JSONObject test1(@RequestBody TestBean testBean) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("id", "12");
-        jsonObject.put("name", "boy");
-        return jsonObject;
-    }
 }
